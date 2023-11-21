cbc48c07240d9e91ac157d3b03dddd1706a08be0
Sami Shehzad <samishehzad1106@gmail.com>
2023-11-21 17:19:47 +0500

    dart import fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gorintest/core/features/auth/bloc/auth_bloc.dart';
-import 'package:gorintest/core/features/auth/ui/login_screen.dart';
 import 'package:gorintest/core/features/auth/ui/splash_screen.dart';
 import 'package:gorintest/firebase_options.dart';
 
